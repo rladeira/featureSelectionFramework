7a01f165e8aaf75f5d03f8c1cb8791c7f6c8b8a6
rladeira <rwladeira@gmail.com>
2015-12-06 21:55:49 -0200

    Deactivate parallel processing for search, by default.

--- featureSelectionMethods/generateFeatureSelectionMethod.R
+++ featureSelectionMethods/generateFeatureSelectionMethod.R
@@ -8,18 +8,18 @@ generateFeatureSelectionMethod <- function(searchMethod,
   if(is.function(searchMethod) == FALSE)
     stop("Invalid Argument! searchMethod must be a function.")
   
-  featureSelectionMethod <- 
-    function(data, labels, ...) {
-      
-      searchResult <- searchMethod(
-        featuresNames = colnames(data), 
-        multivariateCriterion = multivariateCriterion,
-        data = data,
-        labels = labels,
-        ...)
-      
-      return(searchResult)
-    }
+  featureSelectionMethod <- function(data, labels, ...) {
+    
+    searchResult <- searchMethod(
+      featuresNames = colnames(data), 
+      multivariateCriterion = multivariateCriterion,
+      data = data,
+      labels = labels,
+      allowParallel = FALSE,
+      ...)
+    
+    return(searchResult)
+  }
   
   return(featureSelectionMethod)
 }
